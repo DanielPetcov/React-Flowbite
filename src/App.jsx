ef4419babaaf1a5fa395e17961b54815046d3fe0
Daniel Petcov <danielpetcov.work@gmail.com>
2024-07-02 22:39:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import MyPage from './components/Button'
+
+function App() {
+  
+
+  return (
+    <>
+      <MyPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import { Button } from "flowbite-react";
+
+export default function MyPage() {
+  return (
+    <div>
+      <Button>Submit</Button>
+    </div>
+  );
+}
